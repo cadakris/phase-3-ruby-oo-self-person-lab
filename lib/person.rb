c7c61f5341ebd46eacace36e67459c1c86234127
Kristen <kriscadacio@gmail.com>
2022-04-06 09:56:18 -0400

    commit tree 87ce03e256871aeff235ed1b1f6cc9485d190e18

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,17 @@
 # your code goes here
+require 'pry'
+class Person
+
+    attr_reader :name :bank_account
+
+    def initialize(name, bank_account=25, happiness=8, hygiene=8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+end
+
+kristen = Person.new("Kristen")
+kristen.name
+binding.pry
